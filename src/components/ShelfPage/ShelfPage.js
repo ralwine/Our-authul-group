cd9fd0e4233f917fd1cd338e93563d3ce963f1ad
LazyDotExe <papaporo@proton.me>
2023-09-01 11:37:18 -0500

    added delete button/function

--- src/components/ShelfPage/ShelfPage.js
+++ src/components/ShelfPage/ShelfPage.js
@@ -1,7 +1,6 @@
 
 import { useDispatch, useSelector } from "react-redux";
 import React, { useEffect } from "react";
-import React from 'react';
 import { ShelfItem } from './ShelfItem/ShelfItem';
 
 
@@ -21,33 +20,41 @@ function ShelfPage() {
     dispatch({ type: "FETCH_ITEMS" });
   };
 
+  function removeItem(thing) {
+    dispatch({
+      type: "REMOVE_ITEM",
+      payload: thing.id,
+    })
+
+  }
+
 
 
 
 
   return (
-<<<<<<< HEAD
-    <div className ="container">
-=======
+
     <>
     <div className="container">
->>>>>>> f0025328a65a444bbb63622d977d78ff541f94e5
+
       <h2>Shelf</h2>
       <p>All of the available items can be seen here.</p>
       <ul>
         {item.map ( thing => (
-          <li key={thing.id}>{thing.description}</li>
+          <li key={thing.id}>{thing.description}
+          <button onClick={() => removeItem(thing)}>DELETE</button></li>
+          
         ))}
       </ul>
       
 
     </div>
-<<<<<<< HEAD
+
     
-=======
+
     <ShelfItem />
     </>
->>>>>>> f0025328a65a444bbb63622d977d78ff541f94e5
+
   );
 }
 
